8ba9f28480a36031c226c7329436977e80c9e2a2
Manoah0 <78923855+Manoah0@users.noreply.github.com>
2021-02-16 12:12:45 +0100

    Authorization

---
+++ authorization/check permissions.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://api.extremecloudiq.com/auth/permissions/:check"
+
+payload="{\n  \"uri\": \"https://api.extremecloudiq.com/auth/apitoken/info\",\n  \"method\": \"get\"\n}"
+headers = {
+  'Authorization': '***',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
