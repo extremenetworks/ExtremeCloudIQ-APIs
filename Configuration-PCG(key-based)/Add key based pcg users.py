00228a22eff317597f6ff2f7565172c087e81b0e
Manoah0 <78923855+Manoah0@users.noreply.github.com>
2021-05-19 09:52:03 +0200

    Configuration-PCG(key-based)

---
+++ Configuration-PCG(key-based)/Add key based pcg users.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+url = "https://api.extremecloudiq.com/pcg/key-based/{{pcgk_id}}/users"
+
+payload = json.dumps({
+  "users": [
+    {
+      "name": "string",
+      "email": "string",
+      "user_group_name": "string"
+    }
+  ]
+})
+headers = {
+  'accept': 'application/json',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
