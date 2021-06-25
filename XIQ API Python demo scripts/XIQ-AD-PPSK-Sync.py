8f99dfbcf3e0d3285ca5264eb812fa87be038e06
Tim Smith <20387652+timjsmith24@users.noreply.github.com>
2021-06-25 10:36:06 -0400

    Set to sync PPSK users with a specific AD group

--- XIQ API Python demo scripts/XIQ-AD-PPSK-Sync.py
+++ XIQ API Python demo scripts/XIQ-AD-PPSK-Sync.py
@@ -9,12 +9,12 @@ import os
 import logging
 
 
-# setup your settings
-
+# Global Variables - ADD CORRECT VALUES
 server_name = "enter your server name or IP"
 domain_name = "enter you domain name"
 user_name = "enter your username"
 password = "enter your password"
+ad_group = "enter active directory group"
 XIQ_username = "enter your ExtremeCloudIQ Username"
 XIQ_password = "enter your ExtremeCLoudIQ password
 usergroupID = " enter your user group ID"
@@ -28,7 +28,8 @@ logging.basicConfig(
     level=os.environ.get("LOGLEVEL", "INFO"),
     format= '%(asctime)s: %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
-
+# userAccountControl codes used for disabled accounts
+ldap_disable_codes = ['514','66050']
 
 URL = "https://api.extremecloudiq.com"
 headers = {"Accept": "application/json", "Content-Type": "application/json"}
@@ -37,18 +38,30 @@ from ldap3 import Server, Connection, ALL, NTLM, ALL_ATTRIBUTES, ALL_OPERATIONAL
 
 
 
-try:
-    server = Server(server_name, get_info=ALL)
-    conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
-    conn.search('dc={},dc=local,'.format(domain_name), '(objectclass=person)', attributes=['name', 'mail', 'userAccountControl'])
-except:
-    log_msg = f"Unable to reach server {server_name}"
-    logging.error(log_msg)
-    print(log_msg)
-    print("script exiting....")
-    # not having ppsk will break later line - for ldap_entry in conn.entries:
-    raise SystemExit
-
+def retrieveADUsers(ad_group):
+    #Building search base from fqdn
+    subdir_list = fqdn.split('.')
+    tdl = subdir_list[-1]
+    subdir_list = subdir_list[:-1]
+    SearchBase = 'DC=' + ',DC='.join(subdir_list) + ',DC=' + tdl
+    try:
+        server = Server(server_name, get_info=ALL)
+        conn = Connection(server, user='{}\\{}'.format(domain_name, user_name), password=password, authentication=NTLM, auto_bind=True)
+        conn.search(
+                search_base= SearchBase,
+                search_filter='(&(objectClass=user)(memberof:1.2.840.113556.1.4.1941:=cn={},cn=users,{}))'.format(ad_group, SearchBase),
+                search_scope=SUBTREE,
+                attributes = ['objectClass', 'userAccountControl', 'sAMAccountName', 'name', 'mail'])
+        ad_result = conn.entries
+        conn.unbind()
+        return ad_result
+    except:
+        log_msg = f"Unable to reach server {server_name}"
+        logging.error(log_msg)
+        print(log_msg)
+        print("script exiting....")
+        raise SystemExit
+    
 
 
 def GetaccessToken(XIQ_username, XIQ_password):
@@ -110,7 +123,7 @@ def retrievePPSKusers(pageSize):
     ppskusers = []
 
     while page < 1000:
-        url = URL + "/ssids/users?page=" + str(page) + "&limit=" + str(pageSize)
+        url = URL + "/ssids/users?page=" + str(page) + "&limit=" + str(pageSize) + "&user_group_ids=" + usergroupID
         #print("Retrieving next page of PPSK users from ExtremeCloudIQ starting at page " + str(page) + " url: " + url)
 
         # Get the next page of the ppsk users
@@ -126,19 +139,20 @@ def retrievePPSKusers(pageSize):
             logging.warning(f"\t\t{response}")
             raise TypeError(log_msg)
 
-
+    
         rawList = response.json()['data']
         #for name in rawList:
         #    print(name)
         #print("Retrieved " + str(len(rawList)) + " users on this page")
         ppskusers = ppskusers + rawList
+        
 
         if len(rawList) == 0:
             #print("Reached the final page - stopping to retrieve users ")
             break
 
         page = page + 1
-        return ppskusers
+    return ppskusers
 
 
 
@@ -157,87 +171,112 @@ def deleteuser(userId):
         raise TypeError(log_msg)
     elif response.status_code == 200:
         logging.info(f"succesfully deleted PPSK user {userId}")
-        print(f"succesfully deleted PPSK user {userId}")
+        return 'Success'
     #print(response)
 
-# MAIN
-try:
-    login = GetaccessToken(XIQ_username, XIQ_password)
-except TypeError as e:
-    print(e)
-    raise SystemExit
-except:
-    log_msg = "Unknown Error: Failed to generate token"
-    logging.error(log_msg)
-    print(log_msg)
-    raise SystemExit
-
-try:
-    ppsk_users = retrievePPSKusers(100)
-except TypeError as e:
-    print(e)
-    print("script exiting....")
-    # not having ppsk will break later line - if not any(d['name'] == name for d in ppsk_users):
-    raise SystemExit
-except:
-    log_msg = ("Unknown Error: Failed to retrieve users from XIQ")
-    logging.error(log_msg)
-    print(log_msg)
-    print("script exiting....")
-    # not having ppsk will break later line - if not any(d['name'] == name for d in ppsk_users):
-    raise SystemExit
-
-ldap_users = {}
-
-#print("\nParsing all users from LDAP:\n")
-#print(conn.entries)
-
-for ldap_entry in conn.entries:
+def main():
     try:
-        ldap_users[str(ldap_entry.name)] = {
-            "userAccountControl": str(ldap_entry.userAccountControl),
-            "email": str(ldap_entry.mail)}
-
+        login = GetaccessToken(XIQ_username, XIQ_password)
+    except TypeError as e:
+        print(e)
+        raise SystemExit
     except:
-        log_msg = (f"Unexpected error: {sys.exc_info()[0]}")
+        log_msg = "Unknown Error: Failed to generate token"
+        logging.error(log_msg)
+        print(log_msg)
+        raise SystemExit
+
+    try:
+        ppsk_users = retrievePPSKusers(100)
+    except TypeError as e:
+        print(e)
+        print("script exiting....")
+        # not having ppsk will break later line - if not any(d['name'] == name for d in ppsk_users):
+        raise SystemExit
+    except:
+        log_msg = ("Unknown Error: Failed to retrieve users from XIQ")
         logging.error(log_msg)
         print(log_msg)
         print("script exiting....")
-        # not having ppsk will break later line - for name, details in ldap_users.items():
+        # not having ppsk will break later line - if not any(d['name'] == name for d in ppsk_users):
         raise SystemExit
 
+    ldap_users = {}
+    ldap_capture_success = True
+    ad_result = retrieveADUsers(ad_group)
+    #print("\nParsing all users from LDAP:\n")
 
-#print("\nSuccessfully parsed " + str(len(ldap_users)) + " LDAP users\n")
+    for ldap_entry in ad_result:
+        if str(ldap_entry.name) not in ldap_users:
+            try:
+                ldap_users[str(ldap_entry.name)] = {
+                    "userAccountControl": str(ldap_entry.userAccountControl),
+                    "email": str(ldap_entry.mail)}
 
-for name, details in ldap_users.items():
-    if not any(d['name'] == name for d in ppsk_users):
-        try:
-            CreatePPSKuser(name, details["email"])
-        except TypeError as e:
-            print(e)
-        except:
-            log_msg = f"Unknown Error: Failed to create user {name}"
-            logging.error(log_msg)
-            print(log_msg)
-
-    #print("Check account " + name + ": userAccountControl: " + details["userAccountControl"])
-
-    if details["userAccountControl"] == '66050':
-
-        userId=0
-        for user in ppsk_users:
-            if user['name'] == name:
-                userId=user['id']
-                #print("Found user id: " + str(userId) + " --> trying to delete this user")
-                deletePPSKuser=deleteuser(userId)
-                break
-
-        if userId == 0:
-            log_msg = (f"Failed to retrieve data on user by username {name}")
-            logging.error(log_msg)
-            print(log_msg)
+            except:
+                log_msg = (f"Unexpected error: {sys.exc_info()[0]}")
+                logging.error(log_msg)
+                print(log_msg)
+                logging.warning("User info was not captured from Active Directory")
+                logging.warning(f"{ldap_entry}")
+                # not having ppsk will break later line - for name, details in ldap_users.items():
+                ldap_capture_success = False
+                continue
 
 
+    log_msg = "Successfully parsed " + str(len(ldap_users)) + " LDAP users"
+    logging.info(log_msg)
+    #print(f"\n{log_msg}\n")
+
+    ldap_disabled = []
+    for name, details in ldap_users.items():
+        if details['email'] == '[]':
+            print(f"User {name} doesn't have a email set")
+            continue
+        if not any(d['name'] == name for d in ppsk_users) and not any(d == details['userAccountControl'] for d in ldap_disable_codes):
+            try:
+                CreatePPSKuser(name, details["email"])
+            except TypeError as e:
+                log_msg = f"failed to create {name}: {e}"
+                logging.error(log_msg)
+                print(log_msg)
+            except:
+                log_msg = f"Unknown Error: Failed to create user {name}"
+                logging.error(log_msg)
+                print(log_msg)
+        elif any(d == details['userAccountControl'] for d in ldap_disable_codes):
+            ldap_disabled.append(name)
+    
+    # Remove disabled accounts from ldap users
+    for name in ldap_disabled:
+        del ldap_users[name]
+    if ldap_capture_success:
+        for x in ppsk_users:
+            email = x['email_address']
+            xiqid = x['id']
+            # check if any xiq user is not included in active ldap users
+            if not any(d['email'] == email for d in ldap_users.values()):
+                try:
+                    result = deleteuser(xiqid)
+                except TypeError as e:
+                    logmsg = f"Failed to delete user {email}  with error {e}"
+                    logging.error(logmsg)
+                    print(logmsg)
+                    continue
+                except:
+                    log_msg = f"Unknown Error: Failed to create user {email} "
+                    logging.error(log_msg)
+                    print(log_msg)
+                    continue
+                if result == 'Success':
+                    log_msg = f"User {email} was successfully deleted."
+                    logging.info(log_msg)
+                    print(log_msg)  
+    else:
+        log_msg = "No users will be deleted from XIQ because of the error(s) in reading ldap users"
+        logging.warning(log_msg)
+        print(log_msg)
 
 
-
+if __name__ == '__main__':
+	main()
